95a27e0517fc094815d109920a20fba64eb9568a
AwesomeFish <88491244+AwesomeFish@users.noreply.github.com>
2023-04-07 17:33:48 +1000

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,43 +1,18 @@
-const app = Vue.createApp({
-  data() {
-    return {
-      items: [],
-      newItem: {
-        name: '',
-        profilePicture: '',
-        description: '',
-        date: new Date().toLocaleDateString()
-      }
-    }
-  },
-  mounted() {
-    // Load data from JSON file
-    fetch('data.json')
-      .then(response => response.json())
-      .then(data => {
-        this.items = data;
-      })
-      .catch(error => console.error(error));
-  },
-  methods: {
-    addItem() {
-      // Add new item to array and clear form
-      this.items.push(this.newItem);
-      this.newItem = {
-        name: '',
-        profilePicture: '',
-        description: '',
-        date: new Date().toLocaleDateString()
-      };
-      // Update JSON file
-      const json = JSON.stringify(this.items, null, 2);
-      fetch('data.json.then(() => {
-        // Show success message and reload page
-        alert('Item added successfully!');
-        location.reload();
-      })
-      .catch(error => console.error(error));
-  }
-});
 
-app.mount('#app');
+const app = Vue.createApp({
+    data() {
+      return {
+        items: []
+      }
+    },
+    mounted() {
+      // Load data from JSON file
+      fetch('data/data.json')
+        .then(response => response.json())
+        .then(data => {
+          this.items = data;
+        })
+        .catch(error => console.error(error));
+    }
+  });
+  app.mount('#app');
