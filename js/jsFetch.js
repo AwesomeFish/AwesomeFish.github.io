686ca5cfcd2e81154f0fc7790e17a7a71d707094
AwesomeFish <88491244+AwesomeFish@users.noreply.github.com>
2023-04-25 14:49:35 +1000

    Update jsFetch.js

--- js/jsFetch.js
+++ js/jsFetch.js
@@ -6,19 +6,19 @@ const app = Vue.createApp({	})
 		template: `
 		<div class="row" v-for="m in fetch">
 
-            <div class="col-md-auto">
+            <div class="col-md-3">
                   <p>{{ m.code }}</p>
             </div>
 
-            <div class="col-md-auto">
+            <div class="col-md-3">
                 <p>{{ m.desc }}</p>
             </div>
 
-			<div class="col-md-auto">
+			<div class="col-md-3">
                 <p>{{ m.cp }}</p>
             </div>
 
-			<div class="col-md-auto">
+			<div class="col-md-3">
                 <p>{{ m.type }}</p>
 			</div>
 
